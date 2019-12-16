7caf98ed69b64efc2287757a90ed85b03d47b196
Krisztian21 <krisztian.pasztor95@gmail.com>
2019-12-16 11:01:01 +0100

    A Kukac

--- TheGame/TheGame/Program.cs
+++ TheGame/TheGame/Program.cs
@@ -12,11 +12,104 @@ namespace TheGame
         {
             int szelesseg = Console.WindowWidth;
             int magassag = Console.WindowHeight;
-            //Console.Write($"Szélesség: {szelesseg} Magasság: {magassag}");
-
-            Console.SetCursorPosition(szelesseg / 2, magassag / 2);
-            Console.WriteLine("@");
-            Console.ReadLine();
+            //Console.WriteLine($"{szelesseg} {magassag}");
+            int x = szelesseg / 2;
+            int y = magassag / 2;
+            int[] Xkoord = new int[5];
+            int[] Ykoord = new int[5];
+            for(int i=0; i<5;i++)
+            {
+                Xkoord[i] = x + i;
+                Ykoord[i] = y;
+                //Console.SetCursorPosition(Xkoord[i], Ykoord[i]);
+                //Console.Write("@");
+            }
+            Megrajzol(Xkoord, Ykoord);
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            while(cki.Key!=ConsoleKey.Escape)
+            {
+                cki = Console.ReadKey();
+                switch(cki.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear();
+                        Balfele(ref Xkoord, ref Ykoord);
+                        Megrajzol(Xkoord, Ykoord);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Felfele(ref Xkoord, ref Ykoord);
+                        Console.Clear();
+                        Megrajzol(Xkoord, Ykoord);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        Jobbfele(ref Xkoord, ref Ykoord);
+                        Megrajzol(Xkoord, Ykoord);
+                        break;
+                    case ConsoleKey.DownArrow: 
+                        Console.Clear();
+                        Lefele(ref Xkoord, ref Ykoord);
+                        Megrajzol(Xkoord, Ykoord);
+                        break;
+                }
+            }
+            Console.ReadKey(true);
+        }
+        static void Megrajzol(int[] x, int[] y)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.SetCursorPosition(x[i], y[i]);
+                Console.Write("@");
+            }
+        }
+        static void Felfele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetX = x;
+            int[] atmenetY = y;
+            for (int i = 1; i < 4; i++)
+            {
+                x[i] = atmenetX[i + 1];
+                y[i] = atmenetY[i + 1];
+            }
+            x[4] = atmenetX[4];
+            y[4] = atmenetY[4] - 1;
+        }
+        static void Lefele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetX = x;
+            int[] atmenetY = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetX[i + 1];
+                y[i] = atmenetY[i + 1];
+            }
+            x[4] = atmenetX[4];
+            y[4] = atmenetY[4] + 1;
+        }
+        static void Jobbfele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetX = x;
+            int[] atmenetY = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetX[i + 1];
+                y[i] = atmenetY[i + 1];
+            }
+            x[4] = atmenetX[4] + 1;
+            y[4] = atmenetY[4];
+        }
+        static void Balfele(ref int[] x, ref int[] y)
+        {
+            int[] atmenetX = x;
+            int[] atmenetY = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetX[i + 1];
+                y[i] = atmenetY[i + 1];
+            }
+            x[4] = atmenetX[4] - 1;
+            y[4] = atmenetY[4];
         }
     }
 }
