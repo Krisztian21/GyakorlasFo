fc45d4e3400708a8c392f886945e240ced4f6f05
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-01-14 15:12:26 +0100

    kedd 20.01.14

--- Attekintes/Attekintes/Program.cs
+++ Attekintes/Attekintes/Program.cs
@@ -11,18 +11,18 @@ namespace Attekintes
         static void Main(string[] args)
         {
             Hazikedvenc hk = new Hazikedvenc();
-            hk.Neme = "lány";
-            Console.WriteLine($"A házikedvenc neme: {hk.Neme}");
+            hk.neme = "lány";
+            Console.WriteLine($"A házikedvenc neme: {hk.neme}");
             Hazikedvenc hk2 = new Hazikedvenc();
             hk2.Megvaltoztat("fiú");
-            Console.WriteLine($"A házikedvenc neme: {hk2.Neme}");
+            Console.WriteLine($"A házikedvenc neme: {hk2.neme}");
             Console.ReadLine();
         }
     }
     class Hazikedvenc
     {
         //mező adattag
-        private string neme;
+        public string neme;
 
         //mező property-je
         public string Neme {
@@ -30,10 +30,11 @@ namespace Attekintes
             set { this.neme = value; }
         }
 
+        //public string Neme{ get; set; }
         public void Megvaltoztat(string ertek)
         {
             this.neme = ertek;
-            //Console.WriteLine($"A kedvenc neme: {this.neme}.");
+            Console.WriteLine($"A kedvenc neme: {this.neme}");
         }
     }
 }
